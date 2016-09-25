ff65d81fe1f07e78b6443e142eea6fa809d2abfc
Jagged <will.mooar@gmail.com>
2016-09-25 23:46:57 +1300

    Added MatchTests and SwitchTests

---
+++ OneOf.Tests/SwitchTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace OneOf.Tests
+{
+    public class SwitchTests
+    {
+        [Test]
+        public void SwitchCallsBoolActionWhenBool()
+        {
+            var success = false;
+
+            var x = (OneOf<string, bool>)true;
+            x.Switch(
+                str => Assert.Fail(),
+                bln => success = (bln == true));
+            Assert.AreEqual(true, success);
+        }
+
+        [Test]
+        public void SwitchCallsStringActionWhenString()
+        {
+            var success = false;
+
+            var x = (OneOf<string, bool>)"xyz";
+            x.Switch(
+                str => success = (str == "xyz"),
+                bln => Assert.Fail());
+            Assert.AreEqual(true, success);
+        }
+
+        [Test]
+        public void SwitchCallsOtherActionWhenNoMatch()
+        {
+            var success = false;
+
+            var x = (OneOf<string, bool>)"xyz";
+            x.Switch(
+                otherwise: () => success = true);
+            Assert.AreEqual(true, success);
+        }
+    }
+
+}
