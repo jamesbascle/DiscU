2b705b94eb5cc8ce9207b6c958bc8ace5f7001fb
Jagged <will.mooar@gmail.com>
2017-02-07 00:14:22 +1300

    Tidy and comment OneOfBase.

--- OneOf/OneOfBase.cs
+++ OneOf/OneOfBase.cs
@@ -9,54 +9,62 @@ namespace OneOf
     public abstract class OneOfBase<TOneOf>  where TOneOf : OneOfBase<TOneOf>
     {
         /// <summary>The OneOf's value</summary>
-        internal readonly object Value;
+        protected readonly object value;
 
-        /// <summary>The matching OneOf's Tn type that the value is, or is derived from.</summary>
-        internal readonly Type ValueTn;
+        /// <summary>The matching OneOf's Tn type that best matches the value.</summary>
+        protected readonly Type valueTn;
 
         /// <summary>Cache the list of Tns for the OneOf as calling GetGenericArguments is SLOW.</summary>
-        static readonly Type[] OneOfTns = typeof(TOneOf).GetGenericArguments();
+        static readonly Type[] oneOfTns = typeof(TOneOf).GetGenericArguments();
 
-        private static readonly ConcurrentDictionary<Type,Type> TypeToTypeCache = new ConcurrentDictionary<Type, Type>();
+        /// <summary>The OneOf's Tns formatted as comma-separated type names for error reporting</summary>
+        static readonly string oneOfTnsCsv = string.Join(", ", oneOfTns.Select(type => type.Name));
+
+        /// <summary>Cache of best matches between OneOf value type and the OneOf Tn's.</summary>
+        static readonly ConcurrentDictionary<Type,Type> valueTypeToTnCache = new ConcurrentDictionary<Type, Type>();
         
         /// <summary>Ctor</summary>
+        /// <param name="value">The OneOf's value</param>
+        /// <param name="matchedType">The OneOf's Tn that best matches the value if known, otherwise null.</param>
         protected OneOfBase(object value, Type matchedType)
         {
             if (value == null)
                 throw new ArgumentNullException(nameof(value));
 
             var valueType = value.GetType();
-            var valueTn = matchedType == valueType ? valueType : GetBestType(valueType);
-            //var valueTn = GetBestType(valueType);
+            var valueTn = (valueType == matchedType) ? valueType : GetBestType(valueType);
 
             if (valueTn == null )
-            {
-                var oneOfTnCsv = string.Join(", ", OneOfBase<TOneOf>.OneOfTns.Select(type => type.Name));
-                throw new ArgumentException($"Value of type {valueType.Name} is not compatible with OneOf<{oneOfTnCsv}>", nameof(value));
-            }
+                throw new ArgumentException($"Value of type {valueType.Name} is not compatible with OneOf<{oneOfTnsCsv}>", nameof(value));
 
-            this.Value = value;
-            this.ValueTn = valueTn;
-        }
-
-        private static Type GetBestType(Type valueType)
-        {
-            Type ret;
-            if (!TypeToTypeCache.TryGetValue(valueType, out ret))
-            {
-                ret = TypeToTypeCache.GetOrAdd(valueType, MatchClosestTn(valueType));
-            }
-            return ret;
+            this.value = value;
+            this.valueTn = valueTn;
         }
 
         /// <summary>
-        /// find the best matching Tn that value is a subclass of, otherwise null.
+        /// find the best matching Tn for valueType, otherwise null.
         /// </summary>
-        static Type MatchClosestTn(Type valueType)
+        static Type GetBestType(Type valueType)
         {
             Type bestType = null;
 
-            foreach (var tn in OneOfBase<TOneOf>.OneOfTns)
+            // OPTIMISATION
+            // Using TryGetValue with GetOrAdd is 6x faster than GetOrAdd alone(!).
+
+            if (!valueTypeToTnCache.TryGetValue(valueType, out bestType))      
+                bestType = valueTypeToTnCache.GetOrAdd(valueType, GetBestTypeUncached);
+
+            return bestType;
+        }
+
+        /// <summary>
+        /// find the best matching Tn for valueType, otherwise null.
+        /// </summary>
+        static Type GetBestTypeUncached(Type valueType)
+        {
+            Type bestType = null;
+
+            foreach (var tn in oneOfTns)
             {
                 // does the value match this Tn?
                 if (tn.IsAssignableFrom(valueType))
