07f81570d023b8b49d14967e88a4af560e98833c
jamesbascle <rbascle9@gmail.com>
2017-02-15 01:34:25 -0500

    Reimplementing the matching function and the OneOfMatcher for greater performance.  No longer need many objects, just the one.

---
+++ OneOf/IOneOfMatcherTerminator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneOf
+{
+    public interface IOneOfMatcherTerminator<TResult>
+    {
+        TResult Else(TResult defaultValue);
+        TResult Else(Func<object, TResult> createResult);
+        TResult ElseThrow(Func<object, Exception> createException);
+    }
+}
